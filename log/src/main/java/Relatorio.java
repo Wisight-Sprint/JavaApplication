78044ecd07519236e673fc8853db6ebe89ea1759
Laise1209 <laise.almeida@sptech.school>
2024-10-07 18:38:16 -0300

    feat: criação das classes do DER no JAVA

---
+++ log/src/main/java/Relatorio.java
@@ -0,0 +1,47 @@
+package log.src.main.java;
+
+public class Relatorio {
+    private String dataOcorrencia;
+    private String fuga;
+    private Integer cameraCorporal;
+    private String problemasMentais;
+
+    public Relatorio(String dataOcorrencia, String fuga, Integer cameraCorporal, String problemasMentais) {
+        this.dataOcorrencia = dataOcorrencia;
+        this.fuga = fuga;
+        this.cameraCorporal = cameraCorporal;
+        this.problemasMentais = problemasMentais;
+    }
+
+    public String getDataOcorrencia() {
+        return dataOcorrencia;
+    }
+
+    public void setDataOcorrencia(String dataOcorrencia) {
+        this.dataOcorrencia = dataOcorrencia;
+    }
+
+    public String getFuga() {
+        return fuga;
+    }
+
+    public void setFuga(String fuga) {
+        this.fuga = fuga;
+    }
+
+    public Integer getCameraCorporal() {
+        return cameraCorporal;
+    }
+
+    public void setCameraCorporal(Integer cameraCorporal) {
+        this.cameraCorporal = cameraCorporal;
+    }
+
+    public String getProblemasMentais() {
+        return problemasMentais;
+    }
+
+    public void setProblemasMentais(String problemasMentais) {
+        this.problemasMentais = problemasMentais;
+    }
+}
---
+++ log/src/main/java/Vitima.java
@@ -0,0 +1,48 @@
+package log.src.main.java;
+
+public class Vitima {
+    private Integer idade;
+    private String etinia;
+    private String genero;
+    private String armamento;
+
+    public Vitima(Integer idade, String etinia, String genero, String armamento) {
+        this.idade = idade;
+        this.etinia = etinia;
+        this.genero = genero;
+        this.armamento = armamento;
+
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    public String getEtinia() {
+        return etinia;
+    }
+
+    public void setEtinia(String etinia) {
+        this.etinia = etinia;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getArmamento() {
+        return armamento;
+    }
+
+    public void setArmamento(String armamento) {
+        this.armamento = armamento;
+    }
+}
