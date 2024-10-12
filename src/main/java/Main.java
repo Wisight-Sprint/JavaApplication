fbd582c949a170ea75dfe5600314ae4b114edb68
SamuelPazz <samuel.paz@sptech.school>
2024-10-12 20:33:40 -0300

    feat: conexão com o s3, baixando arquivo csv

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,17 @@
 import com.project.config.DBConnectionProvider;
+import com.project.provider.ConnectionProviderS3;
+import com.project.provider.ServiceS3;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 public class Main {
     public static void main(String[] args) {
         DBConnectionProvider dbConnectionProvider = new DBConnectionProvider();
         JdbcTemplate connection = dbConnectionProvider.getDatabaseConnection();
+
+        ConnectionProviderS3 connectionProviderS3 = new ConnectionProviderS3();
+        ServiceS3 serviceS3 = new ServiceS3(connectionProviderS3);
+
+        serviceS3.listBuckets();
+
     }
 }
---
+++ src/main/java/com/project/provider/ModelBucketS3.java
@@ -0,0 +1,9 @@
+package com.project.provider;
+
+import software.amazon.awssdk.services.s3.model.Bucket;
+import software.amazon.awssdk.services.s3.model.S3Object;
+
+public class ModelBucketS3 {
+    public Bucket bucketName;
+    public S3Object objectName;
+}
