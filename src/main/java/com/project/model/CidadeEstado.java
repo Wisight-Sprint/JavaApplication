47209968eb7776b1156675425b1f5b134e91bb4c
CauanAraruna <cauan.araruna@sptech.school>
2024-11-30 20:29:28 -0300

    adição dos metodos de tratativa de dados no nas classes a serem inseridas no banco

--- src/main/java/com/project/model/CidadeEstado.java
+++ src/main/java/com/project/model/CidadeEstado.java
@@ -1,5 +1,13 @@
 package com.project.model;
 
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class CidadeEstado extends Tratativa{
     private String cidade;
     private String estado;
@@ -9,11 +17,6 @@ public class CidadeEstado extends Tratativa{
     public CidadeEstado() {}
 
     public CidadeEstado(CidadeEstado colunaCidadeEstado) {}
-
-    @Override
-    public String tratativaDados() {
-        return null;
-    }
 
     public String getCidade() {
         return cidade;
@@ -38,4 +41,62 @@ public class CidadeEstado extends Tratativa{
     public void setCidade_estado_id(Integer cidade_estado_id) {
         this.cidade_estado_id = cidade_estado_id;
     }
+
+    @Override
+    public void writeLog(String message) throws IOException {
+        writerlog.write(message + "\n");
+        writerlog.flush();
+    }
+
+    @Override
+    public void tratativaDados() throws IOException {
+        try (InputStream inputStream = serviceS3.getObjectInputStream(bucket, xlsxKey);
+             Workbook workbook = new XSSFWorkbook(inputStream)) {
+
+            Sheet sheet = workbook.getSheetAt(0);
+            SimpleDateFormat formato = new SimpleDateFormat("yyyy-MM-dd");
+
+            for (Row row : sheet) {
+                if (row.getRowNum() == 0)
+                    continue;
+
+                Cell cell6 = row.getCell(6);
+                Cell cell7 = row.getCell(7);
+
+
+                Date cellDtOcorrencia;
+
+                cellDtOcorrencia = new Date();
+
+                Date dtLimite;
+
+                dtLimite = formato.parse("2024-01-01");
+
+                if (cellDtOcorrencia.before(dtLimite))
+                    continue;
+
+                String cellCidade = (cell6 != null && cell6.getCellType() == CellType.STRING) ? cell6.getStringCellValue().toUpperCase() : "";
+                String cellEstado = (cell7 != null && cell7.getCellType() == CellType.STRING) ? cell7.getStringCellValue().toUpperCase() : "";
+
+                cellCidade = cellCidade.replaceAll(" ", "");
+                cellEstado = cellEstado.replaceAll(" ", "");
+
+//                colunaCidadeEstado.setCidade(cellCidade);
+//                colunaCidadeEstado.setEstado(cellEstado);
+//
+//                insertIntoDatabase(colunaCidadeEstado);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            writerlog.flush();
+            String logKey = serviceS3.createLogKey();
+            serviceS3.createLog(bucket, logKey, byteArrayOutputStream);
+        }
+
+        System.out.println("-----------\nInserção finalizada");
+        writerlog.close();
+    }
+
 }
--- src/main/java/com/project/model/Tratativa.java
+++ src/main/java/com/project/model/Tratativa.java
@@ -1,5 +1,28 @@
 package com.project.model;
 
-public abstract class  Tratativa {
-    public abstract String tratativaDados();
+import com.project.provider.ConnectionProviderS3;
+import com.project.provider.DBConnectionProvider;
+import com.project.services.ServiceS3;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.io.BufferedWriter;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public abstract class Tratativa {
+
+    ConnectionProviderS3 connectionProviderS3 = new ConnectionProviderS3();
+    ServiceS3 serviceS3 = new ServiceS3(connectionProviderS3);
+
+    String bucket = serviceS3.getFirstBucket();
+    String xlsxKey = serviceS3.getFirstXlsxKey(bucket);
+
+    ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+    BufferedWriter writerlog = new BufferedWriter(new OutputStreamWriter(byteArrayOutputStream));
+
+    public abstract void tratativaDados() throws IOException;
+
+    public abstract void writeLog(String message) throws IOException;
+
 }
